6ef4d8c10edd287fd859ae2a519ef4045db24e2b
unknown <josutic.sutic@gmail.com>
2025-06-11 16:00:07 +0200

    feat(23): test 3 ga

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,16 +1,16 @@
 import ReactGA from 'react-ga4';
 
 const isDev = import.meta.env.DEV;
+const VITE_GA_MEASUREMENT_ID = import.meta.env.VITE_GA_MEASUREMENT_ID;
 
 export const initGA = () => {
-  if (!isDev) {
-    console.log('ga initialized');
-    ReactGA.initialize('G-TRFLXG80MY');
+  if (!isDev && VITE_GA_MEASUREMENT_ID) {
+    ReactGA.initialize(VITE_GA_MEASUREMENT_ID);
   }
 };
 
 export const trackPageview = (path: string) => {
-  if (!isDev) {
+  if (!isDev && VITE_GA_MEASUREMENT_ID) {
     ReactGA.send({ hitType: 'pageview', page: path });
   }
 };
@@ -19,7 +19,8 @@ export const trackEvent = (
   eventName: string,
   params?: Record<string, string | number | boolean>
 ) => {
-  if (!isDev) {
+  if (!isDev && VITE_GA_MEASUREMENT_ID) {
+    console.log('event', params);
     ReactGA.event(eventName, params);
   }
 };
